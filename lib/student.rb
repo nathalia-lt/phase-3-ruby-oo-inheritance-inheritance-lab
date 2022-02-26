a40e991f0173b108ae4ad17b83ee374c1e23b948
nathalia <nathaliatroina@gmail.com>
2022-02-26 18:24:46 -0500

    commit tree ca158f5e432c776ddbf0097bc217d6846d1dae13

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
 
+    attr_reader :knowledge
+    # perguntar se sempre que eu uso initialize eu 
+    #tenho que colocar o attr
+
+
+    def initialize
+        @knowledge = []
+    end
+
+        def learn(knowledge)
+            #Define a method, #learn, that takes in a string and adds 
+            #that string to the student's @knowledge array.
+        @knowledge << knowledge
+        #porque um arroba? acho que porque se trara do attr
+        end
 end
+
+seila = student.new("Steve")
